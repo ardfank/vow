c0ec2f522757bfd641ed5ba6e077307c65417a57
ardfank <hitoriki.batosai@gmail.com>
2022-12-04 05:51:00 +0800

    Nabilah Min 04 Des 2022 05:51:00  WITA

--- sh/scale.sh
+++ sh/scale.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 h=$(/usr/bin/ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 "$1")
+# d=$(/usr/bin/ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 | xargs printf %.0f)
 echo "Create GIF for $1"
 /usr/bin/ffmpeg -y -ss 3 -t 6 -i "$1" -filter_complex "[0:v] fps=9,scale=w=72:h=-2,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -loop 0 -f webp "$1.gif"
 if (($h > 760)); then
